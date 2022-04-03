137e40b5ca00949c2e8f26de1ae32e1f91b1505b
ChamikaVishmal <vishmalwkc@gmail.com>
2022-04-03 16:34:57 +0530

    sample django app with jenkins

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Introduction
+
+This repository contains a simple Python Django Hello-world application and AWS Cloudformation automation code to set up the AWS infrastructure from the scratch.
+
+Both the sample app and the Jenkins service have been dockerized and deployed using AWS ECS. AWS RDS PostgreSQL server is used as the database of the sample web application.
+
+## Installation
+
+Install the [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html) and set up the AWS credentials.
+
+Go to the porject path and run the start.sh file to run the automation
+
+```bash
+sh start.sh
+```
---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Enter AWS Account ID"
+read account_id
+
+echo "Enter AWS Region"
+read region
+
+if [ -z "${account_id}" ] ||  [ -z "${region}" ]; then
+    echo "Inputs cannot be empty"
+    exit 0
+fi
+
+if ! [[ "$account_id" =~ ^[+-]?[0-9]+\.?[0-9]*$ ]]; then 
+    echo "Inputs must be a numbers" 
+    exit 0 
+fi
+
+# AWS ECR login 
+aws ecr get-login-password --region $region | docker login --username AWS --password-stdin $account_id.dkr.ecr.$region.amazonaws.com
+
+# Build
+cd Jenkins && docker build -t jenkins -f Dockerfile .
+
+# Tag
+docker tag jenkins:latest $account_id.dkr.ecr.$region.amazonaws.com/mass-jenkins:latest
+
+# Push
+docker push $account_id.dkr.ecr.$region.amazonaws.com/mass-jenkins:latest
